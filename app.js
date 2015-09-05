8fd70788cebbca360b1e5e6a508f87ae7ca2cb58
Matthew Hsu <gears88@gmail.com>
2015-09-05 12:39:59 -0400

    Support for start date and limit

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var moment = require('moment');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  res.toMsqld = function(date) {
+    return moment(date).format('YYYY-MM-DD');
+  };
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/stock/:symbol', function(req, res, next) {
-  db.query("SELECT * FROM price WHERE symbol=?",[req.params.symbol], function(err, data){
+  var date = res.toMsqld(new Date(req.query.start)) || "1990-02-01";
+  var limit = parseInt(req.query.limit) || 60;
+  console.log(date, limit);
+  db.query("SELECT * FROM price WHERE symbol=? AND date_ex >= ? ORDER BY date_ex ASC LIMIT ?",[req.params.symbol,date,limit], function(err, data){
     res.send(data);
    });
 });
