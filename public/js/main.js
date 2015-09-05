37f15e6c61346c57dee515d2be85196e885d2765
Austin <213reeses@gmail.com>
2015-09-05 17:21:22 -0400

    yay typeahead

--- public/js/main.js
+++ public/js/main.js
@@ -8,14 +8,28 @@ $(document).ready(function() {
   });
 });
 
-var symbolSelect = angular.module('symbolSelect', []);
+var symbolSelect = angular.module('symbolSelect', ['siyfion.sfTypeahead']);
 
 symbolSelect.controller('symbolController', function($scope) {
-  $scope.allTickers = [];
+  var allTickers = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: ['ASDF', 'HJKL', 'QWER', 'TYTU']
+  });
+
+  allTickers.initialize();
+
+  $scope.tickerOptions = {
+    minLength: 3,
+    highlight: true
+  }
+
+  $scope.tickerData = {
+    source: allTickers.ttAdapter()
+  }
+
   $scope.tracks = [];
   $scope.init = function() {
-    // fetch list of tickers
-    $scope.allTickers = ['ABCD', 'DEFG', 'HIJK'];
 
     $scope.tracks = [
     {
@@ -24,8 +38,45 @@ symbolSelect.controller('symbolController', function($scope) {
     },
     {
       title: "Default",
-      tickers: ["ABC", "DEF"]
+      tickers: ["GHI", "JKL"]
     }
     ];
   };
+
+  $scope.delTicker = function(ticker, track) {
+    ticker = ticker.toUpperCase();
+    track.tickers.splice(track.tickers.indexOf(ticker), 1);
+  }
+
+  $scope.delTrack = function(track) {
+    $scope.tracks.splice($scope.tracks.indexOf(track), 1);
+  }
+
+  $scope.addTicker = function(ticker, track) {
+    ticker = ticker.toUpperCase();
+    if (ticker != "" && !_.contains(track.tickers, ticker)) {
+      track.tickers.push(ticker);
+    }
+    $scope.ticker = '';
+  }
+
+  $scope.addTrack = function() {
+    if (this.track != '') {
+      $scope.tracks.push({
+        title: this.track,
+        tickers: []
+      });
+    }
+    $scope.track = '';
+  }
+
+  $scope.keyPress = function(ev, track) {
+    if (ev.keyCode == 13) {
+      var tgt = $(ev.target);
+      $scope.addTicker(tgt.prev().val(), track);
+      tgt.val('');
+      tgt.typeahead('close');
+    }
+  }
+
 });
