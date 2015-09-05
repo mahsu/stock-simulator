7ba13c52fafe4c56e8db610c8f90d2465b9a1a1a
Matthew Hsu <gears88@gmail.com>
2015-09-05 11:47:17 -0400

    Added mysql connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db_con').connection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/stock/:symbol', function(req, res, next) {
+  db.query("SELECT * FROM price WHERE symbol=?",[req.params.symbol], function(err, data){
+    res.send(data);
+   });
+});
+
 
 module.exports = router;
